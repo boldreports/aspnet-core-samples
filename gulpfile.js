e58dad119ad40bba17fce6f31ba8ab40b4db5a0b
vishnu <vishnu.tamilventhan@syncfusion.com>
2024-01-03 22:53:41 +0530

    feature(REPORT-342363): public repo

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const download = require("gulp-download");
 const { createReadStream } = require('fs');
 const { rm, mkdir } = require('shelljs');
 const unzipper = require('unzipper');
-const puppeteerPath = './wwwroot/scripts/puppeteer/Win-901912';
+const puppeteerPath = './wwwroot/puppeteer/Win-901912';
 
 gulp.task('puppeteer-download', (done) => {
     mkdir('-p', `${puppeteerPath}`);
